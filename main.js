9a466aa17b1f5f595348e063eb4048af12775ddc
jakecolder93 <jakecolder93@gmail.com>
2021-08-04 15:34:04 +0100

    Add JS to create smooth scrolling between elements on the same page

---
+++ main.js
@@ -0,0 +1,14 @@
+let anchorLinks = document.querySelectorAll('a[href^="#"]');
+
+for (let item of anchorLinks){
+    item.addEventListener('click', (e)=> {
+        let hashval = item.getAttribute('href')
+        let target = document.querySelector(hashval)
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+        history.pushState(null, null, hashval)
+        e.preventDefault()
+    })
+}
